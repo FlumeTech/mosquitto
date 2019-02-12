04b4467f3b0b2f28ecd618b2c971724ba138cd77
Christian Johansen <johansen.christian.a@gmail.com>
2019-02-12 13:15:38 -0800

    Fixed indentation

--- lib/proxy_mosq.c
+++ lib/proxy_mosq.c
@@ -35,82 +35,82 @@ Support for PROXY v1 from a load balancer.
  * of a PUBREC command, which is always 4 bytes. 
  */
 int proxy__read_header(struct mosquitto *mosq) {
-    uint8_t byte;
-    uint8_t header[5] = {0x50, 0x52, 0x4f, 0x58, 0x59};
-    ssize_t read_length;
-    int8_t proxy_out;
+	uint8_t byte;
+	uint8_t header[5] = {0x50, 0x52, 0x4f, 0x58, 0x59};
+	ssize_t read_length;
+	int8_t proxy_out;
 
-    if (mosq->in_packet.proxy <= 0 || mosq->in_packet.proxy >= PROXY_READING) {
-        do {
-            if (mosq->in_packet.proxy >= PROXY_READING) {
-                /* If reading proxy, try to read the max size minus what we have.
-                 * This may capture some MQTT bytes too, but the parsing code can just
-                 * use the buffer instead of calling net__read.
-                 */
-		if (PROXY_MIN_SIZE - mosq->in_packet.bufSize > 0) {
-			read_length = net__read(mosq, mosq->in_packet.buffer + mosq->in_packet.bufSize, PROXY_MIN_SIZE - mosq->in_packet.bufSize);
-		} else {
-			read_length = net__read(mosq, mosq->in_packet.buffer + mosq->in_packet.bufSize, 1);
-		}
-            } else {
-                read_length = net__read(mosq, &byte, 1);
-            }
-            /* If reading the proxy and we actually got some bytes */
-            if (mosq->in_packet.proxy >= PROXY_READING && read_length > 0) {
-                mosq->in_packet.bufSize += read_length;
-                proxy_out = proxy__verify_header(mosq);
-                if (proxy_out > 0) {
-                    return MOSQ_ERR_SUCCESS;
-                } else if (proxy_out < 0) {
-                    return MOSQ_ERR_PROXY;
-                }
-            }
-            else if (read_length == 1) {
-                /* Throw the byte into the buffer */
-                mosq->in_packet.buffer[mosq->in_packet.bufSize] = byte;
-                mosq->in_packet.bufSize++;
-                /* Check if byte matches header */
-                if (byte == header[mosq->in_packet.proxy * -1]) {
-                    mosq->in_packet.proxy--;
-                }
-                /* Proxy exists if first two bytes match, explained above */
-                if (mosq->in_packet.proxy == -2) {
-                    mosq->in_packet.proxy = PROXY_READING;
-                }
-                /* We have two bytes without matching, so no proxy */
-                else if (mosq->in_packet.bufSize >= 2) {
-                    mosq->in_packet.proxy = PROXY_INVALID;
-                }
-            }
-            else {
-            #ifdef WIN32
-                errno = WSAGetLastError();
-            #endif
-                if(errno == EAGAIN || errno == COMPAT_EWOULDBLOCK){
-                    if(mosq->in_packet.to_process > 1000){
-                        /* Update last_msg_in time if more than 1000 bytes left to
-                            * receive. Helps when receiving large messages.
-                            * This is an arbitrary limit, but with some consideration.
-                            * If a client can't send 1000 bytes in a second it
-                            * probably shouldn't be using a 1 second keep alive. */
-                        pthread_mutex_lock(&mosq->msgtime_mutex);
-                        mosq->last_msg_in = mosquitto_time();
-                        pthread_mutex_unlock(&mosq->msgtime_mutex);
-                    }
-                    return MOSQ_ERR_SUCCESS;
-                }else{
-                    switch(errno){
-                        case COMPAT_ECONNRESET:
-                            return MOSQ_ERR_CONN_LOST;
-                        default:
-                            return MOSQ_ERR_ERRNO;
-                    }
-                }
-            }
-        } while (mosq->in_packet.proxy <= 0 || mosq->in_packet.proxy >= PROXY_READING);
-    }
+	if (mosq->in_packet.proxy <= 0 || mosq->in_packet.proxy >= PROXY_READING) {
+		do {
+			if (mosq->in_packet.proxy >= PROXY_READING) {
+				/* If reading proxy, try to read the max size minus what we have.
+				 * This may capture some MQTT bytes too, but the parsing code can just
+				 * use the buffer instead of calling net__read.
+				 */
+				if (PROXY_MIN_SIZE - mosq->in_packet.bufSize > 0) {
+					read_length = net__read(mosq, mosq->in_packet.buffer + mosq->in_packet.bufSize, PROXY_MIN_SIZE - mosq->in_packet.bufSize);
+				} else {
+					read_length = net__read(mosq, mosq->in_packet.buffer + mosq->in_packet.bufSize, 1);
+				}
+			} else {
+				read_length = net__read(mosq, &byte, 1);
+			}
+			/* If reading the proxy and we actually got some bytes */
+			if (mosq->in_packet.proxy >= PROXY_READING && read_length > 0) {
+				mosq->in_packet.bufSize += read_length;
+				proxy_out = proxy__verify_header(mosq);
+				if (proxy_out > 0) {
+					return MOSQ_ERR_SUCCESS;
+				} else if (proxy_out < 0) {
+					return MOSQ_ERR_PROXY;
+				}
+			}
+			else if (read_length == 1) {
+				/* Throw the byte into the buffer */
+				mosq->in_packet.buffer[mosq->in_packet.bufSize] = byte;
+				mosq->in_packet.bufSize++;
+				/* Check if byte matches header */
+				if (byte == header[mosq->in_packet.proxy * -1]) {
+					mosq->in_packet.proxy--;
+				}
+				/* Proxy exists if first two bytes match, explained above */
+				if (mosq->in_packet.proxy == -2) {
+					mosq->in_packet.proxy = PROXY_READING;
+				}
+				/* We have two bytes without matching, so no proxy */
+				else if (mosq->in_packet.bufSize >= 2) {
+					mosq->in_packet.proxy = PROXY_INVALID;
+				}
+			}
+			else {
+			#ifdef WIN32
+				errno = WSAGetLastError();
+			#endif
+				if(errno == EAGAIN || errno == COMPAT_EWOULDBLOCK){
+					if(mosq->in_packet.to_process > 1000){
+						/* Update last_msg_in time if more than 1000 bytes left to
+							* receive. Helps when receiving large messages.
+							* This is an arbitrary limit, but with some consideration.
+							* If a client can't send 1000 bytes in a second it
+							* probably shouldn't be using a 1 second keep alive. */
+						pthread_mutex_lock(&mosq->msgtime_mutex);
+						mosq->last_msg_in = mosquitto_time();
+						pthread_mutex_unlock(&mosq->msgtime_mutex);
+					}
+					return MOSQ_ERR_SUCCESS;
+				}else{
+					switch(errno){
+						case COMPAT_ECONNRESET:
+							return MOSQ_ERR_CONN_LOST;
+						default:
+							return MOSQ_ERR_ERRNO;
+					}
+				}
+			}
+		} while (mosq->in_packet.proxy <= 0 || mosq->in_packet.proxy >= PROXY_READING);
+	}
 
-    return MOSQ_ERR_SUCCESS;
+	return MOSQ_ERR_SUCCESS;
 }
 
 /* Checks the buffer for a valid proxy header. If may be valid but is not yet,
@@ -122,46 +122,46 @@ int8_t proxy__verify_header(struct mosquitto *mosq) {
 	char af[5];
 	char dest[PROXY_HOST_SIZE];
 	int destPort;
-    char *loc;
-    char *end;
+	char *loc;
+	char *end;
 
-    if (mosq->in_packet.bufSize >= PROXY_MAX_SIZE) {
-        return -1;
-    }
-    if (mosq->in_packet.bufSize >= 8 && memcmp(mosq->in_packet.buffer, "PROXY", 5) == 0) {
-        /* Search for end of proxy line */
-        loc = memchr(mosq->in_packet.buffer, PROXY_CR, mosq->in_packet.bufSize - 1);
-        if (!loc) {
-            return 0;
-        }
-	else if (loc && loc[1] != PROXY_LF) {
-	    return -1;
-	}
-        *(loc - 1) = '\0';
-        end = loc + 2;
-	if (mosq->remote_host == NULL) {
-		mosq->remote_host = mosquitto__malloc(PROXY_HOST_SIZE);
-		if (!mosq->remote_host) return MOSQ_ERR_NOMEM;
-	}
-	if (sscanf((char *) mosq->in_packet.buffer, "PROXY %s %s %s %d %d", af, mosq->remote_host, dest, &(mosq->remote_port), &destPort) != 5) {
+	if (mosq->in_packet.bufSize >= PROXY_MAX_SIZE) {
 		return -1;
 	}
-        if (strcmp(af, "TCP4") == 0) {
-            mosq->remote_af = AF_INET;
-        }
-        else if (strcmp(af, "TCP6") == 0) {
-            mosq->remote_af = AF_INET6;
-        }
-        else {
-            return -1;
-        }
-        /* Shift back buffer for extra data */
-        memmove(mosq->in_packet.buffer, end, mosq->in_packet.bufSize - ((uint8_t *)end - mosq->in_packet.buffer));
-        mosq->in_packet.bufSize -= (uint8_t *)end - mosq->in_packet.buffer;
-	mosq->in_packet.proxy = PROXY_VALID;
+	if (mosq->in_packet.bufSize >= 8 && memcmp(mosq->in_packet.buffer, "PROXY", 5) == 0) {
+		/* Search for end of proxy line */
+		loc = memchr(mosq->in_packet.buffer, PROXY_CR, mosq->in_packet.bufSize - 1);
+		if (!loc) {
+			return 0;
+		}
+		else if (loc && loc[1] != PROXY_LF) {
+			return -1;
+		}
+		*(loc - 1) = '\0';
+		end = loc + 2;
+		if (mosq->remote_host == NULL) {
+			mosq->remote_host = mosquitto__malloc(PROXY_HOST_SIZE);
+			if (!mosq->remote_host) return MOSQ_ERR_NOMEM;
+		}
+		if (sscanf((char *) mosq->in_packet.buffer, "PROXY %s %s %s %d %d", af, mosq->remote_host, dest, &(mosq->remote_port), &destPort) != 5) {
+			return -1;
+		}
+		if (strcmp(af, "TCP4") == 0) {
+			mosq->remote_af = AF_INET;
+		}
+		else if (strcmp(af, "TCP6") == 0) {
+			mosq->remote_af = AF_INET6;
+		}
+		else {
+			return -1;
+		}
+		/* Shift back buffer for extra data */
+		memmove(mosq->in_packet.buffer, end, mosq->in_packet.bufSize - ((uint8_t *)end - mosq->in_packet.buffer));
+		mosq->in_packet.bufSize -= (uint8_t *)end - mosq->in_packet.buffer;
+		mosq->in_packet.proxy = PROXY_VALID;
 
-	log__printf(mosq, MOSQ_LOG_INFO, "New connection has remote address %s on port %i.", mosq->remote_host, mosq->remote_port);
-    }
+		log__printf(mosq, MOSQ_LOG_INFO, "New connection has remote address %s on port %i.", mosq->remote_host, mosq->remote_port);
+	}
 
-    return 0;
+	return 0;
 }
